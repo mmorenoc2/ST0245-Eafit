763ca1df85580035bca90b955dae8268a19d66e6
mmorenoc2 <65234250+mmorenoc2@users.noreply.github.com>
2020-05-12 18:45:50 -0500

    Add files via upload

---
+++ laboratorios/lab05/codigo/Node.java
@@ -0,0 +1,14 @@
+public class Node {    
+    Long id;
+    double x;
+    double y;
+    String name;
+    
+    public Node(Long id) {
+        this.id = id;
+    }
+    public Long getID()
+    {
+      return id;
+    }
+}
